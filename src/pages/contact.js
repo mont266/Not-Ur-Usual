8ccf862526d3c07eda982b06b4e4feb29f5acacb
Jukka Koivu <skuuppi11@hotmail.com>
2020-08-20 14:43:49 +0300

    Update
    Small update with thank you page for contact form.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -35,7 +35,7 @@ export default function Contact() {
       <form
         name="contact"
         method="post"
-        action="/"
+        action="/thanks/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         onSubmit={handleSubmit}
